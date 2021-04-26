d8754cfa9e8aae9a2af7880b41dfefca887606f2
Pavel Kuznetsov <pavel.kuznetsov@aton.ru>
2021-04-26 17:27:08 +0300

    fixes for cocoapods 1.10

--- lib/cocoapods-binary/helper/shared_cache.rb
+++ lib/cocoapods-binary/helper/shared_cache.rb
@@ -13,14 +13,14 @@ module Pod
             #
             # @return [Boolean]
             def self.has?(target, options)
-                self.has_local_cache_for(target, options) || self.has_s3_cache_for(target, options)
+                self.has_local_cache_for?(target, options) || self.has_s3_cache_for?(target, options)
             end
 
             # `true` if there is local cache for the target
             # `false` otherwise
             #
             # @return [Boolean]
-            def has_local_cache_for?(target, options)
+            def self.has_local_cache_for?(target, options)
                 if Podfile::DSL.shared_cache_enabled
                     path = local_framework_cache_path_for(target, options)
                     path.exist?
@@ -33,7 +33,7 @@ module Pod
             # `false` otherwise
             #
             # @return [Boolean]
-            def has_s3_cache_for?(target, options)
+            def self.has_s3_cache_for?(target, options)
                 result = false
                 if Podfile::DSL.shared_s3_cache_enabled
                     s3_cache_path = s3_framework_cache_path_for(target, options)
